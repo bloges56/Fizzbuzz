7b9bb4c622e9f3853acf5f33da2148ed8cd54f3c
bloges56 <brady.drew.logan@gmail.com>
2020-11-17 14:29:42 -0600

    log fizz, buzz, or fizzbuzz

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace Fizzbuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
